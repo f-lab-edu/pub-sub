85397ef3a2886eaed59b11ce45f9eb307f3ca3c8
Hoyun Jung <jhy9094@gmail.com>
2024-03-29 19:40:04 +0900

    chore: auditing 설정 분리
    #10

--- src/main/java/com/stemm/pubsub/PubSubApplication.java
+++ src/main/java/com/stemm/pubsub/PubSubApplication.java
@@ -2,9 +2,7 @@ package com.stemm.pubsub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class PubSubApplication {
 
