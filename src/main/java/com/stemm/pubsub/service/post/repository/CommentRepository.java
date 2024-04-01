c8bcc13298e8d808ac48c055fb98561b82c64a39
Hoyun Jung <jhy9094@gmail.com>
2024-04-01 22:37:26 +0900

    fix: 엔티티 타입 수정
    #14

--- src/main/java/com/stemm/pubsub/service/post/repository/CommentRepository.java
+++ src/main/java/com/stemm/pubsub/service/post/repository/CommentRepository.java
@@ -1,7 +1,7 @@
 package com.stemm.pubsub.service.post.repository;
 
-import com.stemm.pubsub.service.post.entity.post.Post;
+import com.stemm.pubsub.service.post.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Post, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
