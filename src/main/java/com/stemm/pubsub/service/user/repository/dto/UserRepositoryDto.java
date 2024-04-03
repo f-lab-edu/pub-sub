ccd0577bdef1f48249446365521be72afe89c88d
Hoyun Jung <jhy9094@gmail.com>
2024-04-03 14:06:27 +0900

    feat: UserRepositoryDto 필드 변경
    #20

--- src/main/java/com/stemm/pubsub/service/user/repository/dto/UserRepositoryDto.java
+++ src/main/java/com/stemm/pubsub/service/user/repository/dto/UserRepositoryDto.java
@@ -1,15 +1,11 @@
 package com.stemm.pubsub.service.user.repository.dto;
 
-import java.time.LocalDateTime;
-
 public record UserRepositoryDto(
     Long id,
     Long membershipId,
     String nickname,
     String email,
     String profileImageUrl,
-    String bio,
-    LocalDateTime createdDate,
-    LocalDateTime lastModifiedDate
+    String bio
 ) {
 }
