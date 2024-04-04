f56629a53a84212d589be968137b8dc75e842203
Hoyun Jung <jhy9094@gmail.com>
2024-04-04 21:18:11 +0900

    feat: CommentPostRepository 구현
    #19

--- src/main/java/com/stemm/pubsub/service/post/repository/comment/CommentRepository.java
+++ src/main/java/com/stemm/pubsub/service/post/repository/comment/CommentRepository.java
@@ -3,5 +3,5 @@ package com.stemm.pubsub.service.post.repository.comment;
 import com.stemm.pubsub.service.post.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentPostRepository {
 }
