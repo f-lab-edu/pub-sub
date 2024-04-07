4a9add1cbd0363cd423b4b7639be253efe58976c
Hoyun Jung <jhy9094@gmail.com>
2024-04-07 19:38:37 +0900

    feat: PostUserRepository 구현
    #14

--- src/main/java/com/stemm/pubsub/service/post/repository/post/PostRepository.java
+++ src/main/java/com/stemm/pubsub/service/post/repository/post/PostRepository.java
@@ -3,5 +3,5 @@ package com.stemm.pubsub.service.post.repository.post;
 import com.stemm.pubsub.service.post.entity.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostVisibilityRepository {
+public interface PostRepository extends JpaRepository<Post, Long>, PostVisibilityRepository, PostUserRepository {
 }
