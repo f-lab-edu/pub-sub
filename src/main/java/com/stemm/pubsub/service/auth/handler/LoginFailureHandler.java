e81f550b6be9a21a31b5e1b4933700d1795b73f8
Hoyun Jung <jhy9094@gmail.com>
2024-04-15 12:36:51 +0900

    feat: 로그인 성공/실패 핸들러 구현
    #30

---
+++ src/main/java/com/stemm/pubsub/service/auth/handler/LoginFailureHandler.java
@@ -0,0 +1,24 @@
+package com.stemm.pubsub.service.auth.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException exception
+    ) throws IOException {
+        log.info("로그인에 실패했습니다. 메시지: {}", exception.getMessage());
+        response.sendError(SC_UNAUTHORIZED, "로그인에 실패했습니다. 아이디나 비밀번호를 확인해주세요.");
+    }
+}
