b5c682909cbed8c4dec0128e0388847c60e676b6
Hoyun Jung <jhy9094@gmail.com>
2024-04-18 13:14:55 +0900

    feat: 전역 예외 처리 클래스 구현
    #34

---
+++ src/main/java/com/stemm/pubsub/common/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.stemm.pubsub.common;
+
+import com.stemm.pubsub.service.auth.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.stemm.pubsub.service.auth.dto.ApiResponse.error;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Dto validation 예외 처리를 수행하며, 모든 필드 오류를 반환합니다.
+     */
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse<List<String>>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors()
+            .stream()
+            .map(FieldError::getDefaultMessage)
+            .collect(Collectors.toList());
+
+        log.error("validation error: {}", errors);
+
+        return ResponseEntity
+            .badRequest()
+            .body(error("Validation 예외가 발생했습니다.", errors));
+    }
+}
--- src/main/java/com/stemm/pubsub/service/auth/dto/ApiResponse.java
+++ src/main/java/com/stemm/pubsub/service/auth/dto/ApiResponse.java
@@ -6,21 +6,26 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 
 public record ApiResponse<T>(
     @JsonInclude(NON_EMPTY) String message,
-    @JsonInclude(NON_EMPTY) T data
+    @JsonInclude(NON_EMPTY) T data,
+    @JsonInclude(NON_EMPTY) T error
 ) {
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(message, data);
+        return new ApiResponse<>(message, data, null);
     }
 
     public static <T> ApiResponse<T> success(String message) {
-        return new ApiResponse<>(message, null);
+        return new ApiResponse<>(message, null, null);
     }
 
     public static <T> ApiResponse<T> failure(String message, T data) {
-        return new ApiResponse<>(message, data);
+        return new ApiResponse<>(message, data, null);
     }
 
     public static <T> ApiResponse<T> failure(String message) {
-        return new ApiResponse<>(message, null);
+        return new ApiResponse<>(message, null, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message, T error) {
+        return new ApiResponse<>(message, null, error);
     }
 }
