d379d650647abf89a57eca3076f54a0d83e0e6f6
Hoyun Jung <jhy9094@gmail.com>
2024-04-23 22:03:33 +0900

    chore: 원상 복구
    #25

--- src/test/java/com/stemm/pubsub/common/IntegrationTestSupport.java
+++ src/test/java/com/stemm/pubsub/common/IntegrationTestSupport.java
@@ -5,6 +5,7 @@ import com.stemm.pubsub.service.post.service.PostService;
 import com.stemm.pubsub.service.user.repository.membership.MembershipRepository;
 import com.stemm.pubsub.service.user.repository.subscription.SubscriptionRepository;
 import com.stemm.pubsub.service.user.repository.user.UserRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @Transactional
 public abstract class IntegrationTestSupport {
+
+    @Autowired
+    protected EntityManager entityManager;
+
     @Autowired
     protected UserRepository userRepository;
 
